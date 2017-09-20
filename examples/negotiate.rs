0430b43101bde9638da52920468fb985950b6e26
Zibi Braniecki <gandalf@mozilla.com>
2017-09-20 02:07:22 -0400

    Add a negotiation example

---
+++ examples/negotiate.rs
@@ -0,0 +1,10 @@
+extern crate fluent_locale;
+
+use fluent_locale::negotiate_languages;
+
+fn main() {
+    let supported = negotiate_languages(vec!["it", "pl", "ru"],
+                                        vec!["fr", "en-GB", "en-US", "ru", "pl"]);
+
+    println!("{:?}", supported);
+}
