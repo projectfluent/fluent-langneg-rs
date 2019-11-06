e9bfca2a5d62501a971cfe6842dc3dbb34325d34
Emilio Cobos Álvarez <emilio@crisal.io>
2019-11-06 18:05:49 +0100

    Factor out the testing strategy (#19)
    * Easier-to-profile benchmarking.
    
    * Factor out the testing strategy.
    
    And while at it don't bother creating a vec of pairs, just create a single vec
    for the available locales.

--- benches/negotiate.rs
+++ benches/negotiate.rs
@@ -5,6 +5,22 @@ use criterion::Criterion;
 use fluent_langneg::convert_vec_str_to_langids_lossy;
 use fluent_langneg::negotiate_languages;
 
+use unic_langid::LanguageIdentifier;
+
+#[no_mangle]
+#[inline(never)]
+fn do_negotiate<'a>(
+    requested: &[LanguageIdentifier],
+    available: &'a [LanguageIdentifier],
+) -> Vec<&'a LanguageIdentifier> {
+    negotiate_languages(
+        requested,
+        available,
+        None,
+        fluent_langneg::NegotiationStrategy::Filtering,
+    )
+}
+
 fn negotiate_bench(c: &mut Criterion) {
     let requested = &["de", "it", "ru"];
     let available = &[
@@ -16,14 +32,7 @@ fn negotiate_bench(c: &mut Criterion) {
     let available = convert_vec_str_to_langids_lossy(available);
 
     c.bench_function("negotiate", |b| {
-        b.iter(|| {
-            negotiate_languages(
-                &requested,
-                &available,
-                None,
-                fluent_langneg::NegotiationStrategy::Filtering,
-            );
-        })
+        b.iter(|| do_negotiate(&requested, &available))
     });
 }
 
